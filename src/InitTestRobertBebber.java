f6ed2835cbb24f67b902d4cf095c0a01b878824d
robertlbebber <robert.l.bebber@gmail.com>
2017-09-28 14:24:23 -0400

    Create foundations for testing git implementation.

---
+++ src/InitTestRobertBebber.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by trez_ on 9/28/2017.
+ */
+public class InitTestRobertBebber {
+    @Test
+    public void test() {
+        System.out.println("This is a test for seeing if the basics works.");
+    }
+}
